e5b5149edc19a98882d9dc3d6e8432ce4dcaf0d5
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-07-06 17:58:07 +0530

    Login UI is ready

--- my-angular14-app/src/app/app-routing.module.ts
+++ my-angular14-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  // Add future routes here like:
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path: 'user', component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
