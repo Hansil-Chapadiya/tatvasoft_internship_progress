06bbacd7bd5bb137e1944d68add3e009d7993869
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-07-08 09:46:46 +0530

    user guard added

---
+++ my-angular14-app/src/app/guards/user.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+
+    constructor(private router: Router) { }
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            this.router.navigate(['/login']);
+            return false;
+        }
+
+        try {
+            const payload = JSON.parse(atob(token.split('.')[1]));
+
+            if (payload?.role?.toLowerCase() === 'user') {
+                return true;
+            }
+        } catch (e) {
+            console.error('Invalid token format', e);
+        }
+
+        this.router.navigate(['/login']);
+        return false;
+    }
+
+}
